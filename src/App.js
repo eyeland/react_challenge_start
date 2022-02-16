431123f085f526662ba27a2cffa476ee1767172a
eyeland <63192606+eyeland@users.noreply.github.com>
2022-02-16 10:10:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import {React, useState} from "react";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom";
+
+
+import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
+import { ContactsPage } from "./containers/contactsPage/ContactsPage";
+
+function App() {
+  /*
+  Define state variables for 
+  contacts and appointments 
+  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+
+  const ROUTES = {
+    CONTACTS: "/contacts",
+    APPOINTMENTS: "/appointments",
+  };
+
+  /*
+  Implement functions to add data to
+  contacts and appointments
+  */
+  const newContact = (obj) => {
+    setContacts([...contacts, obj])
+  }
+  const newAppointment = (obj) => {
+    setAppointments([...appointments, obj])
+  }
+
+
+  return (
+    <>
+      <nav>
+        <NavLink to={ROUTES.CONTACTS} activeClassName="active">
+          Contacts
+        </NavLink>
+        <NavLink to={ROUTES.APPOINTMENTS} activeClassName="active">
+          Appointments
+        </NavLink>
+      </nav>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to={ROUTES.CONTACTS} />
+          </Route>
+          <Route path={ROUTES.CONTACTS}>
+             {/* Add props to ContactsPage */}
+            <ContactsPage newConact={newContact} contacts={contacts} />
+          </Route>
+          <Route path={ROUTES.APPOINTMENTS}>
+            {/* Add props to AppointmentsPage */}
+            <AppointmentsPage newAppointment={newAppointment} appointments={appointments} />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,38 @@
+import { props } from "bluebird";
+import { React, useEffect, useState } from "react";
+
+
+export const ContactsPage = () => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const [contactInfo, setContactInfo] = useState();
+  //const [duplicateCheck, setDuplicateCheck] = useState();
+  const { newContact, contacts } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2> 
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+      </section>
+    </div>
+  );
+};
