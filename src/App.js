1a41948e698d9fa3448092cad39ab89bada64ff3
eyeland <63192606+eyeland@users.noreply.github.com>
2022-02-17 16:15:48 -0500

    step 6 done *crash on adding new contact*

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([{name: 'bryant', phone: '1800-800-8888', email: 'bryant404@yahoo.com'},
+          {name: 'lisa', phone: '1800-765-9012', email: 'lisa200@aol.com'}]);
   const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = (props) => {
+  const { obj } = props;
+  const objValues = Object.values(obj);
+  
+
+
   return (
     <div className="tile-container">
-      
+      {objValues.map((item, i) => {
+        return(
+          <p key={i} className={ i === 0 ? 'tile-title' : 'tile' } >{item}</p>
+        )
+      })}
     </div>
   );
 };
