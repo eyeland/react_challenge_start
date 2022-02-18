70649b9b36a94dc2ca6cd7b900ebe11140590796
eyeland <63192606+eyeland@users.noreply.github.com>
2022-02-18 12:17:57 -0500

    add regex for phone number

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -38,9 +38,9 @@ export const ContactForm = (props) => {
           onChange={handleChange}
           />
 
-        <input type='tel'  placeholder='phone number' 
+        <input type='tel'  placeholder='phone number Ex. 800-555-5556' 
           name='phone'
-          //pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" fix number
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" 
           value={props.contactInfo.phone}
           onChange={handleChange}
           />
